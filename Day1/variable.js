92feb38f78adfedab20058afdee5b8ddcc15c229
Jemila Ibrahim <jemila.ibrahim@wemabank.com>
2022-10-12 00:34:48 +0100

    Coders Den day 1

---
+++ Day1/variable.js
@@ -0,0 +1,40 @@
+console.log(typeof 'Jemeelah') // string
+console.log(typeof false) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+// Declare four variables without assigning values
+let soldierName = '' // name of soldier
+let soldierRank = '' // rank of soldier
+let soldierSkills = '' // skills of soldier
+let soldierHeight = '' // height of soldier
+
+console.log(soldierName, soldierRank, soldierSkills, soldierHeight)
+
+// Declaring different variables of different data types
+let nameOfChild = 'Benz' // name of kid
+let countryOfBirth = 'Algeria' // country of birth
+let cityOfResidence = 'Tokyo' //  city of residence
+let ageOfkid = 10 // age in years
+
+console.log(nameOfChild, countryOfBirth, cityOfResidence, ageOfkid)
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'Jemeelah' // first name of a person
+let lastName = 'Ibrahim' // last name of a person
+let country = 'Germany' // country
+let city = 'Helsinki' // capital city
+let age = 17 // age in years
+let isMarried = false
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let nameOfPerson = 'Grace', job = 'Software Developer', live = 'Germany'
+console.log(nameOfPerson, job, live)
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+var myAge = 20
+var yourAge = 25
+
+console.log(myAge, yourAge)
